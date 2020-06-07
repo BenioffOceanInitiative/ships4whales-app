c7be9fe0747f9e68d6268c1daca27e5f6feda83d
Ben Best <ben@ecoquants.com>
2020-06-07 23:03:14 +0000

    api-test working on api.whalesafe.net

--- api-test/server.R
+++ api-test/server.R
@@ -2,12 +2,16 @@ shinyServer(function(input, output) {
   
   get_query <- eventReactive(input$btnQuery, {
     
+    simplify <- isolate(input$simplify)
     mmsi     <- isolate(input$mmsi)
     date_beg <- isolate(input$date_beg)
     date_end <- isolate(input$date_end)
     bbox     <- isolate(input$bbox)
     
     q <- c()
+    
+    if (simplify > "")
+      q <- c(q, glue("simplify={input$simplify}"))
     
     if (mmsi > "")
       q <- c(q, glue("mmsi={input$mmsi}"))
@@ -25,7 +29,7 @@ shinyServer(function(input, output) {
   })
   
   get_url <- reactive({
-    url <- "http://api.ships4whales.org/ship_segments"
+    url <- "http://api.whalesafe.net/ship_segments"
     
     q <- get_query()
     
@@ -53,9 +57,28 @@ shinyServer(function(input, output) {
     
     d <- get_data()
     
-    leaflet(d) %>%
-      addProviderTiles(providers$Esri.OceanBasemap) %>%
-      addPolylines()
+    #write_sf(d, "data/tmp_segs.geojson")
+    #d <- read_sf("data/tmp_segs.geojson")
+    
+    speed_bins <- c("[0,10]", "(10,12]", "(12,15]", "(15,Inf]")
+    d$speed_bin_txt <- factor(
+      speed_bins[d$speed_bin_num], speed_bins, ordered = T)
+ 
+    pal <- colorFactor("YlOrRd", d$speed_bin_txt, ordered = T)
+    
+    leaflet() %>%
+      addProviderTiles(
+        providers$Esri.OceanBasemap,
+        options = providerTileOptions(
+          opacity = 0.7)) %>%
+      addPolylines(
+        data  = d,
+        color = ~pal(speed_bin_txt),
+        label = ~glue("mmsi {mmsi} @ {timestamp_beg}: {speed_bin_txt} knots"),
+        opacity = 0.7) %>%
+      addLegend(
+        data = d,
+        pal  = pal, values = ~speed_bin_txt, title = "Speed bin (knots)")
 
   })
   
--- api-test/ui.R
+++ api-test/ui.R
@@ -4,9 +4,12 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      textInput("mmsi"    , "mmsi (eg 248896000)"),
-      textInput("date_beg", "date_beg (eg 2019-10-01)", value = "2019-10-01"),
-      textInput("date_end", "date_end (eg 2019-10-07)", value = "2019-10-07"),
+      textInput("simplify"    , "simplify (eg 1km)", value = "1km"),
+      textInput("mmsi"    , "mmsi (eg 367104060)"),
+      #textInput("date_beg", "date_beg (eg 2019-10-01)", value = "2019-10-01"),
+      #textInput("date_end", "date_end (eg 2019-10-07)", value = "2019-10-07"),
+      textInput("date_beg", "date_beg (eg 2019-10-01)"),
+      textInput("date_end", "date_end (eg 2019-10-07)"),
       textInput("bbox"    , "bbox (eg -121.0,33.3,-117.5,34.6)"),
       actionButton("btnQuery", "Submit query", icon = icon("refresh"))),
     
